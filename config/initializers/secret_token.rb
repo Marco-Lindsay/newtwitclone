7f533f54d9686e7be39b5ac59d1e89e9ec3676b2
marco-lindsay <lindsay_marco@hotmail.com>
2014-03-24 10:48:08 -0700

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitclone::Application.config.secret_key_base = '2471ecde170339ea92ec4816ff8614f05ee445dfa715049329699a488e33d765433f4b5280d0d9a58534bde906d70815a43da1060f57144ec508a79027bcc7fa'
