886415a832253ee973aca7692d8749942736a4f2
marco-lindsay <lindsay_marco@hotmail.com>
2014-03-24 14:01:20 -0700

    strong params working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
             redirect_to new_user_session_path
         else
         @post = Post.new
-        followees_ids = current_user.followees(User).pluck(:id)
-        @activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User")
+       # followees_ids = current_user.followees(User).pluck(:id)
+        #@activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User")
         end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
 
   def show
   end
+
+private 
+
+# Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit({post_ids: []},  :name)
+    end
+
 end
