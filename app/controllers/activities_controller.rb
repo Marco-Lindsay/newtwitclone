daff6f005a06ac30002fde201ca8c33e3cdb9e90
marco-lindsay <lindsay_marco@hotmail.com>
2014-03-26 18:41:26 -0700

    changed navbar and views before final class

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,10 +5,10 @@ class ActivitiesController < ApplicationController
   end
 
 def show
-@activities = PublicActivity::Activity.order("created_at desc").where(current_user.follows?(followable_id))
-	if condition
+@activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.follower_id)
+	# if condition
 	
-	end
+	# end
 end
 
 end
